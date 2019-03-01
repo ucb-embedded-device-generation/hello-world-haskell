88cb04b03b526c9ab63e25f412f5e0da0bebd902
nikhilJain17 <nikhil.jain0987@gmail.com>
2019-02-28 23:14:59 -0800

    learning about stack, the haskell build tool

--- hello-world/app/Main.hs
+++ hello-world/app/Main.hs
@@ -8,3 +8,11 @@ main = do
 	putStrLn("Please enter your name.")
 	name <- getLine
 	putStrLn("Hello " ++ name)
+
+
+-- Some useful stack commands
+-- 1. stack new: create a new project
+-- 2. stack build: build your project
+-- 3. stack exec: run your executable
+-- 4. stack test: run your test suites
+-- 5. 
--- hello-world/src/Lib.hs
+++ hello-world/src/Lib.hs
@@ -2,5 +2,8 @@ module Lib
     ( someFunc
     ) where
 
+import Acme.Missiles
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- someFunc = putStrLn "someFunc"
+someFunc = launchMissiles
